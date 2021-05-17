a749d5ff2513f33f369143b44b91f4b2b9cbbe75
Albert Jiang <albertjly@gmail.com>
2021-05-16 18:49:16 -0700

    make all links and routes

--- controllers/htmlRoutes/dashboard-routes.js
+++ controllers/htmlRoutes/dashboard-routes.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 
+const posts = [
+  {
+    title: 'Object Oriented Programming',
+    date: new Date(),
+    post: false,
+    content: 'Object Oriented programming (OOP) is a programming paradigm that relies on the concept of classes and objects. It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.'
+  },
+  {
+    title: 'Department of Motor Vehicle',
+    date: new Date(),
+    post: false,
+    content: 'Objectf gsf gsdf gsdfg se ds sdf gert'
+  }
+]
+
 router.get('/', (req, res) => {
+  const postData = {
+    dashboardPosts: posts
+  };
   
-  res.render('dashboard');
+  res.render('dashboard', postData);
 });
 
 module.exports = router;
--- controllers/htmlRoutes/index.js
+++ controllers/htmlRoutes/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const homeRoute = require('./home-routes');
 const dashboardRoute = require('./dashboard-routes');
+const loginRoute = require('./login-routes');
+const signinRoute = require('./signin-routes');
 
 router.use('/', homeRoute);
 router.use('/dashboard', dashboardRoute);
+router.use('/login', loginRoute);
+router.use('/signin', signinRoute);
 
 
 module.exports = router;
