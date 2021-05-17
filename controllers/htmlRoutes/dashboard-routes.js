67d52dd6abbf24ff4b52fd22fe2e56f1005d6179
Albert Jiang <albertjly@gmail.com>
2021-05-16 21:00:18 -0700

    install moment.js

--- controllers/htmlRoutes/dashboard-routes.js
+++ controllers/htmlRoutes/dashboard-routes.js
@@ -1,26 +1,25 @@
 const router = require('express').Router();
+const moment = require('moment');
 
 const posts = [
   {
+    post: true,
     title: 'Object Oriented Programming',
-    date: new Date(),
-    post: false,
-    content: 'Object Oriented programming (OOP) is a programming paradigm that relies on the concept of classes and objects. It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.'
-  },
-  {
-    title: 'Department of Motor Vehicle',
-    date: new Date(),
-    post: false,
-    content: 'Objectf gsf gsdf gsdfg se ds sdf gert'
+    date: moment().format("YYYY/MM/DD -- hh:mm:ss a"),
+    content: 'Object Oriented programming (OOP) is a programming paradigm that relies on the concept of classes and objects.'
   }
-]
+];
 
 router.get('/', (req, res) => {
-  const postData = {
-    dashboardPosts: posts
-  };
+  const postsFilter = posts.filter(data => {
+    if(data.post == true) return data; 
+  });
   
-  res.render('dashboard', postData);
+  res.render('dashboard', {
+    page_title: 'Dashboard',
+    blog_title: 'My dashboard',
+    dashboardPosts: postsFilter
+  });
 });
 
 module.exports = router;
