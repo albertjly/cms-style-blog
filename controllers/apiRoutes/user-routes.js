3a7e32357556a2935c23d6f6d08b80a5b6a8671b
Albert Jiang <albertjly@gmail.com>
2021-05-19 00:20:42 -0700

    add post router

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -10,6 +10,36 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (!user) {
+            res.status(400).json({ error: 'Not Found' });
+        }
+
+        const isAuthorized = user.checkPassword(req.body.password);
+
+        if (!isAuthorized) {
+            res.status(400).json({ error: 'Not Found' });
+        }
+
+        req.session.save(() => {
+            req.session.user_id = user.id;
+            req.session.email = user.email;
+            req.session.logged_in = true;
+            console.log(user);
+            res.json(user);
+        })
+    } catch (err) {
+        res.status(400).json(err);
+    }
 })
 
 module.exports = router;
