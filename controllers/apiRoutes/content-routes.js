8a79eea054e5e427f947ecf7c9253bbfc652de05
Albert Jiang <albertjly@gmail.com>
2021-05-23 22:16:25 -0700

    done with content routes

--- controllers/apiRoutes/content-routes.js
+++ controllers/apiRoutes/content-routes.js
@@ -22,4 +22,37 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    // expects => {content_text: "This is the content", user_id: 1, post_id: 2}
+    Content.create({
+        content_text: req.body.content_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbContentData => res.json(dbContentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    Content.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbContentData => {
+            if (!dbContentData) {
+                res.status(404).json({ message: 'No Content found with this id!' });
+                return;
+            }
+            res.json(dbContentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
